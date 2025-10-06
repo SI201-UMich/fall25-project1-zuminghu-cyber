28a2bd9ed57b9f50e753f552f606569a1c8bb6e5
zuminghu-cyber <zuminghu@umich.edu>
2025-10-06 13:41:33 +0800

    Update si201_proj1_zuminghu.py

--- si201_proj1_zuminghu.py
+++ si201_proj1_zuminghu.py
@@ -6,21 +6,36 @@
 # Collaborators: Used GenAI to debug
 # =========================================================
 
-def read_file(filename):
+def load_file(filename):
     try:
+        dict = {}
         f = open(filename, 'r')
-        return f
+        first_row = f.readline()
+        seperated = first_row.split(',')
+        for key in seperated[1:]:
+            dict[key.strip('\n').strip('"')] = []
+        print(dict)
+
+        col = 1
+        for idx in dict.keys():
+            for line in f:
+                dict[idx].append(line.split(',')[col].strip('\n').strip('"'))
+            col += 1
+        print(dict)
+
+
+        return dict
     except FileNotFoundError:
         print(f"read file failed")
         return None
 
 def file_information(file):
     # file information
-    first_row = file.readline()
-    
+    #first_row = file.readline()
+
     pass
 
 def main():
-    file = read_file("penguins.csv")
+    file = load_file("penguins.csv")
     file_information(file)
 main()
