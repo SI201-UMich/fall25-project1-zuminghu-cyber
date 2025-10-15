5910f0b98ac86662e0d01befe0189faf3a8f1169
zuminghu-cyber <zuminghu@umich.edu>
2025-10-15 05:19:05 -0400

    1

--- si201_proj1_zuminghu.py
+++ si201_proj1_zuminghu.py
@@ -40,16 +40,50 @@ def file_information(dict):
     print("\nSample row: ", end = '')
     for key in dict.keys():
         print(dict[key][0], end = ' ')
-        
+    
+    print(dict['body_mass_g'])
+    print(len(dict['body_mass_g']))
     pass
 
-def generate_report():
+def cal_ave_mass(dict):
+    # Average body mass for male penguins on Torgersen island
+    total = 0
+    count = 0
+    for i in range(len(dict['body_mass_g'])):
+        if (dict['body_mass_g'][i] != 'NA') and (dict['sex'][i] == 'male') and (dict['island'][i] == 'Torgersen'):
+            total += int(dict['body_mass_g'][i])
+            count += 1
+    return total / count
+
+def cal_length_diff(dict):
+    # The diﬀerence between the average bill length for diﬀerence species on Dream island?
+    num_chinstrap = 0
+    total_len_chinstrap = 0
+    total_len_adelie = 0
+    num_adelie = 0
+    for i in range(len(dict['island'])):
+        if (dict['island'][i] == 'Dream') and (dict['bill_length_mm'][i] != 'NA'):
+            if dict['species'][i] == 'Chinstrap':
+                num_chinstrap += 1
+                total_len_chinstrap += float(dict['bill_length_mm'][i])
+            if dict['species'][i] == 'Adelie':
+                num_adelie += 1
+                total_len_adelie += float(dict['bill_length_mm'][i])
+    ave_len_chinstrap = total_len_chinstrap / num_chinstrap
+    ave_len_adelie = total_len_adelie / num_adelie
+    diff = ave_len_chinstrap - ave_len_adelie
+    return diff
+
+def generate_report(dict):
     f = open("output.txt", "w")
-    f.write("Hello world")
+    ave_mass = cal_ave_mass(dict)
+    f.write(f"Average body mass for male penguins on Torgersen island is {ave_mass:.2f} grams.\n")
+    diff = cal_length_diff(dict)
+    f.write(f"The diﬀerence between the average bill length for Chinstrap and Adelie on Dream island is {diff:.2f} mm.\n")
     f.close()
 
 def main():
-    file = load_file("penguins.csv")
-    file_information(file)
-    generate_report()
+    dict = load_file("penguins.csv")
+    file_information(dict)
+    generate_report(dict)
 main()
